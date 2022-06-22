e3d71c6af0655adeef185d349f04104867774b5f
Thomas Haug <thomas.kempphaug@gmail.com>
2022-06-22 12:19:33 +0200

    Implemented bishop logic, and testing if it works.

---
+++ lib/bishop.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# Bishop class
+# Contains the bishop and its movement method
+class Bishop
+  attr_accessor :position, :color
+
+  def initialize(position, color)
+    @position = position
+    @color = color
+  end
+
+  def legal_move?(new_position)
+    return false if new_position.nil?
+    (0..7).each do |i|
+      return true if [@position[0]+i, @position[1]+i] == new_position
+      return true if [@position[0]-i, @position[1]+i] == new_position
+      return true if [@position[0]+i, @position[1]-i] == new_position
+      return true if [@position[0]-i, @position[1]-i] == new_position
+    end
+    false
+  end
+
+  def move(new_position)
+    @position = new_position if legal_move?(new_position)
+  end
+
+end
